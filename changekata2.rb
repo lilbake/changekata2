73db1571d0f08267e7174420fdde1ae5aa835c69
lilbake <loko55bass@gmail.com>
2016-02-20 15:10:48 -0500

    test2
    test2

---
+++ changekata2.rb
@@ -0,0 +1,3 @@
+def change_made(amount)
+	coins = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => amount}
+end
--- testchangekata2.rb
+++ testchangekata2.rb
@@ -1,10 +1,10 @@
-require "minitest/autorun"
+require 'minitest/autorun'
 
 require_relative "changekata2.rb"
 
 class MakeChange <Minitest::Test
 
-	def test_that_change_equal_to_one
-		assert_equal({"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 1}(1))
+	def test_that_change_equal_to_zero
+		assert_equal({"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}, change_made(0))
 	end
 end
