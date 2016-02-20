4aca7f34e76221a6b25e571a455c181f57fe256d
lilbake <loko55bass@gmail.com>
2016-02-20 16:45:08 -0500

    refactor to 12 tests
    refactor to 12 tests

--- changekata2.rb
+++ changekata2.rb
@@ -1,31 +1,22 @@
 def change_made(amount)
-	coins = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}
-	
-	
-	if amount >= 20
-		coins["dimes"] = 2
-		amount = amount - 20
-		coins["pennies"] = amount
+	result = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}
+	dimes = 10
+	nickels = 5
+	pennies = 1
 
-	elsif amount >= 15
-		coins["dimes"] = 1
-		coins["nickels"] = 1
-		amount = amount - 15
-		coins["pennies"] = amount
-
-	elsif amount >= 10
-	 	coins["dimes"] = 1
-	 	amount = amount - 10
-	 	coins["pennies"] = amount
-
-
-	elsif amount >= 5
-		coins["nickels"] = 1
-		amount = amount - 5
-		coins["pennies"] = amount
-
-	else 
-	    coins["pennies"] = amount 
+	while amount >= dimes
+		result["dimes"] = result["dimes"] + 1
+		amount = amount - dimes
 	end
-	coins
+
+	if amount >= nickels
+		result["nickels"] = result["nickels"] + 1
+		amount = amount - nickels
+	end
+
+	if amount >= pennies
+		result["pennies"] = amount
+	end
+
+	result
 end
