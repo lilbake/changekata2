3f6d6ad38e9a2486882c8c4cea0607572d4524dc
lilbake <loko55bass@gmail.com>
2016-02-20 17:00:34 -0500

    test 14 refactor
    test 14 refactor

--- changekata2.rb
+++ changekata2.rb
@@ -1,27 +1,14 @@
 def change_made(amount)
 	result = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}
-	quarters = 25
-	dimes = 10
-	nickels = 5
-	pennies = 1
+	coins = {"quarters" => 25, "dimes" => 10, "nickels" => 5, "pennies" => 1}
 
-	while amount >= quarters
-		result["quarters"] = result["quarters"] + 1
-		amount = amount - quarters
-	end
 
-	while amount >= dimes
-		result["dimes"] = result["dimes"] + 1
-		amount = amount - dimes
-	end
+	coins.each do |coin, value|
 
-	while amount >= nickels
-		result["nickels"] = result["nickels"] + 1
-		amount = amount - nickels
-	end
-
-	if amount >= pennies
-		result["pennies"] = amount
+		while amount >= value
+			result[coin] = result[coin] + 1
+			amount = amount - value
+		end
 	end
 
 	result
