e74769d5657fa2ce08b1095c2497c1b62f49b681
lilbake <loko55bass@gmail.com>
2016-02-20 16:18:55 -0500

    test 9
    test 9

--- changekata2.rb
+++ changekata2.rb
@@ -1,7 +1,13 @@
 def change_made(amount)
 	coins = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}
 	
-	 if amount >= 10
+	if amount >= 15
+		coins["dimes"] = 1
+		coins["nickels"] = 1
+		amount = amount - 15
+		coins["pennies"] = amount
+
+	 elsif amount >= 10
 	 	coins["dimes"] = 1
 	 	amount = amount - 10
 	 	coins["pennies"] = amount
