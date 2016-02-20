0ab033ed941a46cfc22a7b00aaff2770197ecd83
lilbake <loko55bass@gmail.com>
2016-02-20 15:47:22 -0500

    test 4
    test 4

--- changekata2.rb
+++ changekata2.rb
@@ -1,3 +1,10 @@
 def change_made(amount)
-	coins = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => amount}
+	coins = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}
+	
+	if amount == 5
+		coins["nickels"] = 1
+	else 
+	    coins["pennies"] = amount 
+	end
+	coins
 end
--- testchangekata2.rb
+++ testchangekata2.rb
@@ -16,6 +16,9 @@ class MakeChange <Minitest::Test
 		assert_equal({"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 2}, change_made(2))
 	end
 
+	def test_that_change_equal_to_five
+		assert_equal({"quarters" => 0, "dimes" => 0, "nickels" => 1, "pennies" => 0}, change_made(5))
+	end
 
 
 
