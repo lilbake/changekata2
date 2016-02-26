1b91496d7bb60a0d79c875045df152d8339471ab
lilbake <loko55bass@gmail.com>
2016-02-25 20:45:02 -0500

    random pairs test3
    test3 rp

--- randompairs.rb
+++ randompairs.rb
@@ -1,4 +1,16 @@
 def create_random_pairs(array_of_students)
-	array = [array_of_students]
+
+	if array_of_students.count <= 2
+		array = [array_of_students]
+	else 
+		random_student_1 = array_of_students.sample
+		array_of_students.delete(random_student_1)
+		random_student_2 = array_of_students.sample
+		array_of_students.delete(random_student_2)
+
+		first_pair = [random_student_1, random_student_2]
+
+		total_pair = [first_pair, array_of_students] 
+	end
 end
 
--- testrandompairs.rb
+++ testrandompairs.rb
@@ -11,6 +11,16 @@ class RandomPairs <Minitest::Test
 	def test_with_two_students_equal_one_pair
 		array_of_pairs = create_random_pairs(["jennifer", "shane"])
 		assert_equal(1, array_of_pairs.count)
+		assert_equal(2, array_of_pairs[0].count)
 	end
+
+	def test_number_of_students_equal_four
+		array_of_pairs = create_random_pairs(["jennifer", "shane", "meg", "aaron"])
+		assert_equal(2, array_of_pairs.count)
+		assert_equal(2, array_of_pairs[0].count)
+	end
+
+
+
 end
 
